d61dd6f91d8252f46b2fef2451cf36a527091065
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-24 04:20:26 +0000

    Delete unnecessary comments in the Pokemon class

--- pokemon.py
+++ pokemon.py
@@ -1,13 +1,6 @@
 from all_pokemon_info import allPokemon
 
-
 class Pokemon:
-    # Pokemon main attributes
-
-    # Stats (HP, Attack, Defense etc)
-    # Moves (Tackle, Water Gun etc)
-    # Type (Water, Fire, Grass etc)
-    # Level
 
     def __init__(self, name):
 
@@ -16,10 +9,6 @@ class Pokemon:
         self.stats = allPokemon[name]['stats']
         self.moves = allPokemon[name]['moves']
         self.level = 5
-        # self.current_xp = 0
-        # self.level = self.load_last_state()['level']
-        # self.current_xp = self.load_last_state()['current_xp']
-        # self.level_up_xp = 50
 
     def say_name(self):
         print(self.name)
