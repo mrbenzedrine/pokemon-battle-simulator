f61bd92af08a07e4adf9ab9f20f39acd2db95f71
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-24 04:45:41 +0000

    Delete trivial methods of the Pokemon class

--- pokemon.py
+++ pokemon.py
@@ -9,15 +9,3 @@ class Pokemon:
         self.stats = allPokemon[name]['stats']
         self.moves = allPokemon[name]['moves']
         self.level = 5
-
-    def say_name(self):
-        print(self.name)
-
-    def show_stats(self):
-        print(self.stats)
-
-    def show_moves(self):
-        print(self.moves)
-
-    def show_level(self):
-        print(self.level)
