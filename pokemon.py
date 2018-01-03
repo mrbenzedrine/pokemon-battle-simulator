96b3e94d942c96fa749b7fe278da13ae2da0857f
Yousef Moazzam <mrbenzedrine@live.co.uk>
2018-01-03 04:23:16 +0000

    Edit use_physical_move to round move_damage to the nearest integer

--- pokemon.py
+++ pokemon.py
@@ -19,7 +19,7 @@ class Pokemon:
 
     def use_physical_move(self, move, damage_multiplier, enemy_pokemon):
 
-        move_damage = (move['Power'] * damage_multiplier) / 5
+        move_damage = round(move['Power'] * damage_multiplier / 5)
         enemy_pokemon.subtract_hp(move_damage)
 
     def subtract_hp(self, damage):
