7cb8cd3daaa20a7580d55599ba8a821959e35066
Yousef Moazzam <mrbenzedrine@live.co.uk>
2018-01-19 17:54:51 +0000

    Add party.py file

---
+++ party.py
@@ -0,0 +1,32 @@
+def user_choose_pokemon_to_switch_to(party):
+
+    pokemon_names = [pokemon.name for pokemon in party]
+
+    while True:
+        print('Please choose a Pokemon to switch out to')
+        for pokemon_name in pokemon_names:
+            print(pokemon_name)
+        print('\n')
+        choice = input()
+
+        if choice in pokemon_names:
+            chosen_pokemon_party_index = pokemon_names.index(choice)
+            break
+        else:
+            print('Invalid Pokemon chosen')
+
+    return chosen_pokemon_party_index
+
+def enemy_choose_pokemon_to_switch_to(party):
+
+    # Just send out the first Pokemon in party with non-zero HP
+
+    for index, pokemon in enumerate(party):
+        if pokemon.stats['HP'][0] > 0:
+            chosen_pokemon_party_index = index
+
+    return chosen_pokemon_party_index
+
+def switch_pokemon(party, index_a, index_b):
+
+    party[index_a], party[index_b] = party[index_b], party[index_a]
